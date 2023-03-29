b34766244f9ae200b83ddcba2a4e4f1f6448895d
Marc Cataford <mcat@riseup.net>
2023-03-28 21:24:44 -0400

    feat: negation support (#4)
    * feat: negation support
    
    * test: coverage for not, update old coverage

--- src/expect.ts
+++ src/expect.ts
@@ -7,21 +7,36 @@ class TestAssertionFailed extends Error {
 	}
 }
 
-// TODO(mcat): This should just be using `assert`
 class Expectation<ValueType> {
 	value: ValueType
+	negated: boolean
 
 	constructor(value: ValueType) {
 		this.value = value
+		this.negated = false
+	}
+
+	/*
+	 * Negates the expectation.
+	 */
+	get not() {
+		this.negated = !this.negated
+		return this
 	}
 
 	toEqual(value: ValueType) {
-		assert.deepEqual(this.value, value, new TestAssertionFailed(`NotEqual! ${this.value} != ${value}`))
+		if (this.negated) {
+			assert.notDeepEqual(this.value, value, new TestAssertionFailed(`Equal! ${this.value} = ${value}`))
+		} else {
+			assert.deepEqual(this.value, value, new TestAssertionFailed(`NotEqual! ${this.value} != ${value}`))
+		}
 	}
 
 	toBe(value: ValueType) {
-		if (Object.is(this.value, value)) return
-		throw new TestAssertionFailed(`NotEqual! ${this.value} !== ${value}`)
+		const isSame = Object.is(this.value, value)
+
+		if ((isSame && !this.negated) || (!isSame && this.negated)) return
+		throw new TestAssertionFailed(`NotEqual! ${this.value} ${this.negated ? '===' : '!=='} ${value}`)
 	}
 }
 
