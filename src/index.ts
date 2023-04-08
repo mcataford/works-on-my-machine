2bc6d945072cf2805c75858e1def9c1fd6adc717
Marc Cataford <mcat@riseup.net>
2023-04-07 23:14:15 -0400

    feat: each support (#13)
    * feat: add each support for test, describe
    
    * docs: document test
    
    * feat: label templating basic support
    
    * refactor: group test exports together
    
    * refactor: leverage each in repetitive tests
    
    * chore: pinned node -> alias
    
    * test: coverage for it, test

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as test, it } from './testComponents/test'
+export { default as describe } from './testComponents/describe'
+export { default as expect } from './testComponents/expect'
