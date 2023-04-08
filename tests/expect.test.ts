51c8279c6e131d3588463fb5586503ef4928e3d2
Marc Cataford <mcat@riseup.net>
2023-04-07 22:58:10 -0400

    refactor: leverage each in repetitive tests

--- tests/expect.test.ts
+++ tests/expect.test.ts
@@ -1,8 +1,6 @@
 import assert from 'assert'
 import { describe, test, expect } from '../src'
 
-type ArbitraryValue = string | number | boolean
-
 describe('Equality', () => {
 	test.each([1, 'expectations', true])('Equality (value=%s)', (value: unknown) => {
 		assert.doesNotThrow(() => expect(value).toEqual(value))
@@ -15,35 +13,27 @@ describe('Equality', () => {
 	])('Equality (failed - values=%s)', (...pair: Array<unknown>) => {
 		assert.throws(() => expect(pair[0]).toEqual(pair[1]))
 	})
-})
-
-describe('Identity', () => {
-	test('Identity comparison (number)', () => {
-		assert.doesNotThrow(() => expect(1).toBe(1))
-	})
-
-	test('Identity comparison (boolean)', () => {
-		assert.doesNotThrow(() => expect(true).toBe(true))
-	})
-
-	test('Identity comparison (string)', () => {
-		assert.doesNotThrow(() => expect('identity').toBe('identity'))
-	})
-
-	test('Identity comparison (failed - number)', () => {
-		assert.throws(() => expect(1).toEqual(2))
-	})
-
-	test('Identity comparison (failed - boolean)', () => {
-		assert.throws(() => expect(false).toBe(true))
-	})
-
-	test('Identity comparison (failed - string)', () => {
-		assert.throws(() => expect('yes').toBe('no'))
-	})
 
 	test('Equality negation', () => {
 		assert.doesNotThrow(() => expect('yes').not.toEqual('no'))
+	})
+})
+
+describe('Identity', () => {
+	test.each([1, true, 'identity'])('Identity comparison (value=%s)', (value: unknown) => {
+		assert.doesNotThrow(() => expect(value).toBe(value))
+	})
+
+	test.each([
+		[1, 2],
+		[false, true],
+		['yes', 'no'],
+	])('Identity comparison (failed - value=%s)', (...pair: Array<unknown>) => {
+		assert.throws(() => expect(pair[0]).toBe(pair[1]))
+	})
+
+	test('Identity negation', () => {
+		assert.doesNotThrow(() => expect('yes').not.toBe('no'))
 	})
 })
 
