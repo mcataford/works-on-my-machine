d8534039c9dbe880d33241d085fb086b891a9e3f
Marc Cataford <mcat@riseup.net>
2023-04-07 23:12:16 -0400

    test: coverage for it, test

---
+++ tests/it.test.ts
@@ -0,0 +1,14 @@
+import assert from 'assert'
+
+import { it, test, expect, describe } from '../src'
+
+describe.each([it, test])('Runs tests', (fn: unknown) => {
+	const testFn = fn as typeof test
+	testFn('Runs a test', () => {
+		assert.doesNotThrow(() => expect(1).toEqual(1))
+	})
+
+	testFn.each([1, 2, 3])('Supports parametrization (value=%s)', (value: unknown) => {
+		assert.doesNotThrow(() => expect(value).toEqual(value))
+	})
+})
