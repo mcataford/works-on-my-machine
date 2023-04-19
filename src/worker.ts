9a7ee9dd8e755a5ff3b6b66aff7490275d20ad1b
Marc Cataford <mcat@riseup.net>
2023-04-18 22:56:39 -0400

    chore: add docs

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,3 @@
-import path from 'path'
-
-import { getContext, spawnProcess } from './utils'
-
-// TODO: What should be message protocol / format be?
-function formatMessage(results: string, failed: boolean): string {
-	return JSON.stringify({ results, failed })
-}
-
 /*
  * Worker runtime
  *
@@ -17,6 +8,28 @@ function formatMessage(results: string, failed: boolean): string {
  * Each worker process is responsible for as many test files as the runner
  * decides to assign it and files assigned to the worker are only
  * touched by the worker assigned to them.
+ */
+
+import path from 'path'
+
+import { getContext, spawnProcess } from './utils'
+
+function formatMessage(results: string, failed: boolean): string {
+	return JSON.stringify({ results, failed })
+}
+
+/*
+ * Entrypoint for the worker.
+ *
+ * Retrieves paths assigned to the worker from the arguments passed when
+ * calling the worker runtime and spawns processes to run the test file
+ * pointed at by each of the paths.
+ *
+ * This will spawn one process per file and each process will communicate back
+ * to the worker's parent process as it finishes.
+ *
+ * If the `TS` flag is passed, the worker runs the test file using ts-node
+ * for Typescript compatibility.
  */
 async function work() {
 	if (process?.send === undefined) throw Error('No process global found')
